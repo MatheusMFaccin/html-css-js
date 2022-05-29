ada6e55ccc1ab8ec079bee29b002793538874d23
Matheus Machado Faccin <96211501+MatheusMFaccin@users.noreply.github.com>
2022-05-29 11:55:26 -0300

    Delete js e html directory

--- js e html/aprendendo.js
+++
@@ -1,9 +0,0 @@
-let list = document.getElementById("list");
-let listnum = parseInt(list.getAttribute("num"));
-
-let cont = "";
-for(let i=0; i<listnum;i++){
-    cont+= "<li></li>";
-
-}
-list.innerHTML = cont;
